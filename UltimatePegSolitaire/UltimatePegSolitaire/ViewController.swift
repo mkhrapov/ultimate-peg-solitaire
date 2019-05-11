b4bdbe433bd74b1fa178a244447e50a11074280f
Maksim Khrapov <mkhrapov@users.noreply.github.com>
2019-05-11 17:22:38 -0500

    Skeleton iOS app?

---
+++ UltimatePegSolitaire/UltimatePegSolitaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UltimatePegSolitaire
+//
+//  Created by Maksim Khrapov on 5/11/19.
+//  Copyright © 2019 Maksim Khrapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UltimatePegSolitaire/UltimatePegSolitaireTests/UltimatePegSolitaireTests.swift
@@ -0,0 +1,34 @@
+//
+//  UltimatePegSolitaireTests.swift
+//  UltimatePegSolitaireTests
+//
+//  Created by Maksim Khrapov on 5/11/19.
+//  Copyright © 2019 Maksim Khrapov. All rights reserved.
+//
+
+import XCTest
+@testable import UltimatePegSolitaire
+
+class UltimatePegSolitaireTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
