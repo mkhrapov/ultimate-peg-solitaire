a1c69eddeaca0df17fac4a709b829a7db349a6d4
Maksim Khrapov <mkhrapov@users.noreply.github.com>
2019-11-16 16:17:19 -0600

    Solve view basic function. Has bugs.

--- UltimatePegSolitaire/SolveViewController.swift
+++ UltimatePegSolitaire/SolveViewController.swift
@@ -30,6 +30,7 @@ final class SolveViewController: UIViewController {
     @IBOutlet weak var pruningNumberTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var solveButton: UIButton!
     
     var gameState: GameState?
     
@@ -42,6 +43,9 @@ final class SolveViewController: UIViewController {
         backItem.title = "Solve"
         navigationItem.backBarButtonItem = backItem
         
+        solveButton.layer.cornerRadius = 10
+        solveButton.clipsToBounds = true
+        
         gameState = GlobalStateManager.shared.getCurrentPlayingBoard()
         setResultsLabel()
         setPruningNumberTextField()
@@ -49,13 +53,26 @@ final class SolveViewController: UIViewController {
     
     
     func setResultsLabel() {
-        if let gameState = gameState, let _ = gameState.board.solution {
-            let time = gameState.board.timeToSolveSeconds
-            let prefix = gameState.board.complementary ? "Complementary" : "A"
-            resultLabel.text = "\(prefix) solution has been found in \(time) sec"
+        resultLabel.attributedText = nil
+        
+        if let gameState = gameState {
+            let time = String(format: "%.2f", gameState.board.timeToSolveSeconds)
+            
+            if let _ = gameState.board.solution {
+                let prefix = gameState.board.complementary ? "Complementary" : "A"
+                resultLabel.text = "\(prefix) solution has been found in \(time) sec."
+            }
+            else {
+                if gameState.board.timeToSolveSeconds > 0.0 {
+                    resultLabel.text = "No solution has been found in \(time) sec. Try using a larger pruning number."
+                }
+                else {
+                    resultLabel.text = "No solution has been found in \(time) sec."
+                }
+            }
         }
         else {
-            resultLabel.text = "No solution has been found in 0.0 sec"
+            resultLabel.text = ""
         }
     }
     
@@ -71,10 +88,74 @@ final class SolveViewController: UIViewController {
     }
     
     
+    func setError(_ text: String) {
+        resultLabel.text = nil
+        let attrs = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        resultLabel.attributedText = NSAttributedString(string: text, attributes: attrs)
+    }
     
     
     
-    @IBAction func solveButton(_ sender: UIButton) {
+    
+    
+    @IBAction func solveButtonAction(_ sender: UIButton) {
+        if let gameState = gameState,
+           let text = pruningNumberTextField.text,
+           let pruningNumber = Int(text),
+           pruningNumber > 0 && pruningNumber <= 10_000
+        {
+            // TODO persist
+            gameState.board.pruningNumber = pruningNumber
+            print("PruningNumber: \(pruningNumber)")
+            activityIndicator.startAnimating()
+            
+            DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive).async {
+                
+                let p = gameState.board.initialPosition()
+                let pruningSearch = PruningSearch(p)
+                pruningSearch.prune(pruningNumber)
+                
+                // TODO timer
+                let numSolutions = pruningSearch.search()
+                let timer = 0.0
+                // TODO TIMER
+                
+                if numSolutions > 0 {
+                    var complementary = false
+                    var history: [Move]?
+                    
+                    for i in 0..<numSolutions {
+                        let p1 = pruningSearch.getSolution(i)
+                        if p1.isComplement(p) {
+                            complementary = true
+                            history = p1.history
+                            break
+                        }
+                    }
+                    
+                    if !complementary {
+                        history = pruningSearch.getSolution(0).history
+                    }
+                    
+                    DispatchQueue.main.async {
+                        gameState.board.complementary = complementary
+                        gameState.board.solution = history
+                        self.setResultsLabel()
+                        self.activityIndicator.stopAnimating()
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        gameState.board.timeToSolveSeconds = timer
+                        self.setResultsLabel()
+                        self.activityIndicator.stopAnimating()
+                    }
+                }
+            }
+        }
+        else {
+            setError("Pruning number must be an integer between 1 and 10000")
+        }
     }
     
 
